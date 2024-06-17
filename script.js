7c0c1b3a1ea35951db0000a993ed65013f7769aa
¡17 GitHub! <jedoles13@gmail.com>
2024-06-17 20:00:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+function toggleSidebar() {
+    var sidebar = document.getElementById('sidebar');
+    var mainContent = document.getElementById('mainContent');
+    sidebar.classList.toggle('active');
+    mainContent.classList.toggle('shifted');
+}
+
 function listItemClicked() {
     // Currently, this function does nothing when a list item is clicked.
     console.log("List item clicked");
